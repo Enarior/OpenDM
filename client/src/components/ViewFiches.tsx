5cce68d26f9e0cab9e30eba292bb893a90f7c4d4
TheoFaEnsibs <falsquelle.e2201075@etud.univ-ubs.fr>
2023-06-05 20:08:14 +0200

    IT WORKS

--- client/src/components/ViewFiches.tsx
+++ client/src/components/ViewFiches.tsx
@@ -10,16 +10,18 @@ import {
   Group,
   Text,
   PasswordInput,
-    Flex,
+  Flex,
 } from "@mantine/core";
 import { SquarePlus, LayoutNavbarExpand } from "tabler-icons-react";
 import { useNavigate } from "react-router-dom";
-import { Global } from '@mantine/core';
+import { Global } from "@mantine/core";
 import "../App.css";
 function ViewFiches(createFiche: any) {
-  return (
-    <Text>MEC TA REUSSI</Text>
-  );
+  if (createFiche.createFiche === true) {
+    return <Text>MEC TA REUSSI</Text>;
+  } else {
+    return <Text>MEC TA REUSSIazeaze</Text>;
+  }
 }
 
 export default ViewFiches;
